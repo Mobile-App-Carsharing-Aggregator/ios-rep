ba8231256004641bdb68225fc08f6d1cb392e2c7
Sbat2000 <zevzok@gmail.com>
2023-11-14 21:00:56 +0800

    Initial Commit

---
+++ Carsharing Aggregator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Carsharing Aggregator
+//
+//  Created by Aleksandr Garipov on 14.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
