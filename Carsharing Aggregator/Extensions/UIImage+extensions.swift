d9116362a58142dede5073bd6be90860f8297ada
Vitaly <anpilov.vitaly@gmail.com>
2023-11-26 21:40:49 +0300

    add Images

---
+++ Carsharing Aggregator/Extensions/UIImage+extensions.swift
@@ -0,0 +1,44 @@
+//
+//  UIImage+extensions.swift
+//  Carsharing Aggregator
+//
+//  Created by Vitaly Anpilov on 26.11.2023.
+//
+
+import UIKit
+
+extension UIImage {
+    
+    static let backward = UIImage(systemName: "chevron.left")
+    static let forward = UIImage(resource: .chevronForward)
+    static let add = UIImage(systemName: "plus")
+    static let faceId = UIImage(systemName: "faceid")
+    static let selected = UIImage(systemName: "chevron.down.circle")
+    static let notSelected = UIImage(systemName: "circlebadge")
+    static let ruble = UIImage(systemName: "rublesign")
+    static let bonus = UIImage(resource: .bonusLogo)
+    static let checkmarkSmall = UIImage(resource: .checkmarksmall)
+    static let pointBlack = UIImage(resource: .ellipsBlack)
+    static let pointGrey = UIImage(resource: .ellipsGrey)
+    static let circleArrows = UIImage(resource: .roundArrows)
+    static let checkmarkGreen = UIImage(resource: .checkmarkAccess)
+    static let closeLightGrey = UIImage(resource: .closeFill)
+    static let cardImage = UIImage(resource: .card)
+    static let car = UIImage(resource: .carModel)
+    static let fuelPump = UIImage(resource: .fuel)
+    static let footPrints = UIImage(resource: .foots)
+    static let starRating = UIImage(resource: .starsmall)
+    static let locationMark = UIImage(resource: .locationmark)
+    static let swithLight = UIImage(resource: .light)
+    static let starFeedback = UIImage(resource: .starbig)
+    
+    // tabBar icons
+    static let tabProfile = UIImage(resource: .profile)
+    static let tabCarSearch = UIImage(resource: .carSearch)
+    static let tabFilters = UIImage(resource: .filters)
+    static let tabOrder = UIImage(resource: .order)
+    static let tabLocation = UIImage(systemName: "location")
+    
+
+}
+
