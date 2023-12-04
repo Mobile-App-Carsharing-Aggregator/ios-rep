8bc3e0943ad58d12b7921829d2c5abb8413cb002
Lobanova Viktoria <va_klevtsova@mail.ru>
2023-12-04 22:31:38 +0300

    add mock point

--- Carsharing Aggregator/Map+TabBar/Views/TabBarViewController.swift
+++ Carsharing Aggregator/Map+TabBar/Views/TabBarViewController.swift
@@ -18,12 +18,38 @@ final class TabBarViewController: UIViewController {
     }
     
     private var carsLocation = [
-        YMKPoint(latitude: 55.751280, longitude: 37.629724),
-        YMKPoint(latitude: 54.751389, longitude: 36.629410),
-        YMKPoint(latitude: 55.751266, longitude: 38.629710),
-        YMKPoint(latitude: 55.751293, longitude: 35.629728),
-        YMKPoint(latitude: 54.751281, longitude: 36.629726)]
-    
+        YMKPoint(latitude: 55.744389, longitude: 37.598402),
+        YMKPoint(latitude: 55.739630, longitude: 37.594764),
+        YMKPoint(latitude: 55.742048, longitude: 37.595123),
+        YMKPoint(latitude: 55.744743, longitude: 37.586652),
+        YMKPoint(latitude: 55.714843, longitude: 37.565741),
+        YMKPoint(latitude: 55.714797, longitude: 37.565668),
+        YMKPoint(latitude: 55.718651, longitude: 37.568095),
+        YMKPoint(latitude: 55.724193, longitude: 37.557187),
+        YMKPoint(latitude: 55.726254, longitude: 37.578652),
+        YMKPoint(latitude: 55.733881, longitude: 37.538283),
+        YMKPoint(latitude: 55.732776, longitude: 37.537228),
+        YMKPoint(latitude: 55.843126, longitude: 37.486103),
+        YMKPoint(latitude: 55.850115, longitude: 37.480832),
+        YMKPoint(latitude: 55.840922, longitude: 37.461481),
+        YMKPoint(latitude: 55.840329, longitude: 37.455382),
+        YMKPoint(latitude: 55.857654, longitude: 37.487533),
+        YMKPoint(latitude: 55.971617, longitude: 37.452978),
+        YMKPoint(latitude: 55.970018, longitude: 37.445775),
+        YMKPoint(latitude: 55.968566, longitude: 37.457891),
+        YMKPoint(latitude: 55.978824, longitude: 37.387044),
+        YMKPoint(latitude: 55.930342, longitude: 37.749893),
+        YMKPoint(latitude: 55.922967, longitude: 37.753279),
+        YMKPoint(latitude: 55.924097, longitude: 37.757589),
+        YMKPoint(latitude: 55.922662, longitude: 37.766441),
+        YMKPoint(latitude: 55.921749, longitude: 37.773823),
+        YMKPoint(latitude: 55.921536, longitude: 37.765449),
+        YMKPoint(latitude: 55.917371, longitude: 37.757016),
+        YMKPoint(latitude: 55.916393, longitude: 37.755586),
+        YMKPoint(latitude: 55.916393, longitude: 37.755586),
+        YMKPoint(latitude: 55.915692, longitude: 37.759201),
+    ]
+  
     private var mapView: YMKMapView = YBaseMapView().mapView
     private lazy var map: YMKMap = mapView.mapWindow.map
     private var pinsCollection: YMKMapObjectCollection?
@@ -119,15 +145,28 @@ extension TabBarViewController: TabViewDelegate {
     }
     
     func locationButtonTapped() {
-        map.move(
-            with: YMKCameraPosition(
-                target: Const.point,
-                zoom: 10,
-                azimuth: 0,
-                tilt: 0
-            ),
-            animation: YMKAnimation(type: YMKAnimationType.smooth, duration: 5),
-            cameraCallback: nil)
+//        map.move(
+//            with: YMKCameraPosition(
+//                target: Const.point,
+//                zoom: 10,
+//                azimuth: 0,
+//                tilt: 0
+//            ),
+//            animation: YMKAnimation(type: YMKAnimationType.smooth, duration: 5),
+//            cameraCallback: nil)
+        let offset: Float = 100
+        let focus = YMKScreenRect(
+            topLeft: YMKScreenPoint(x: offset, y: offset),
+            bottomRight: YMKScreenPoint(
+                x: Float(mapView.mapWindow.width()) - offset,
+                y: Float(mapView.mapWindow.height()) - offset * 3
+            )
+        )
+        let geometry = YMKGeometry(polyline: YMKPolyline(points: carsLocation))
+        let position = map.cameraPosition(with: geometry, azimuth: 0, tilt: 0, focus: focus)
+        map.move(with: position,
+                 animation: YMKAnimation(type: YMKAnimationType.smooth, duration: 1),
+                 cameraCallback: nil)
     }
 }
 
