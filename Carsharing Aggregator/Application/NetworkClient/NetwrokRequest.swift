62b1d743d2404e244026aa7846076c21457093b8
Sbat2000 <zevzok@gmail.com>
2023-12-11 21:36:00 +0800

    add networkClient

---
+++ Carsharing Aggregator/Application/NetworkClient/NetwrokRequest.swift
@@ -0,0 +1,26 @@
+//
+//  NetwrokRequest.swift
+//  Carsharing Aggregator
+//
+//  Created by Aleksandr Garipov on 11.12.2023.
+//
+
+import Foundation
+
+enum HttpMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+}
+
+protocol NetworkRequest {
+    var endpoint: URL? { get }
+    var httpMethod: HttpMethod { get }
+    var dto: Encodable? { get }
+}
+
+extension NetworkRequest {
+    var httpMethod: HttpMethod { .get }
+    var dto: Encodable? { nil }
+}
